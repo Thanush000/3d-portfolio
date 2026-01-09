c07595c1c7bff0d9da0bb96bd7fc15221900d53b
Thanush000 <23r21a1289@mlrit.ac.in>
2026-01-09 13:42:16 +0530

    Remove Twitter/X button from hero section

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -11,7 +11,7 @@ import {
 import { usePreloader } from "../preloader";
 import { BlurIn, BoxReveal } from "../reveal-animations";
 import ScrollDownIcon from "../scroll-down-icon";
-import { SiGithub, SiLinkedin, SiX } from "react-icons/si";
+import { SiGithub, SiLinkedin } from "react-icons/si";
 import { config } from "@/data/config";
 
 import SectionWrapper from "../ui/section-wrapper";
@@ -110,14 +110,6 @@ const HeroSection = () => {
                     </TooltipContent>
                   </Tooltip>
                   <div className="flex items-center h-full gap-2">
-                    <Link
-                      href={config.social.twitter}
-                      target="_blank"
-                    >
-                      <Button variant={"outline"}>
-                        <SiX size={24} />
-                      </Button>
-                    </Link>
                     <Link
                       href="https://github.com/Thanush000"
                       target="_blank"
